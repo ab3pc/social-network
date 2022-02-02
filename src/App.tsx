d84ae78b64b03c912daa9aec5c58ae1a4a3939e8
Aleksandr Borysenko <3pc@ukr.net>
2022-02-02 16:43:57 +0200

    added some components, refactored app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Aside from "./components/Aside";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <Aside />
+      <Profile />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header:React.FC = () => {
+  return (
+    <header className="header">
+      <img
+        className="img header__logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Bxe2zD6NESfM2nc_GjF2WECqbhv86tPZnA&usqp=CAU"
+        alt="LOGO"
+      />
+    </header>
+  );
+};
+
+export default Header;
